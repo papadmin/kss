4e467ff9a8b91b0ab635b209ed0ea3522d1a7946
aysulu <aysulu@Lenovo-G770>
2015-11-24 17:00:43 +0300

    Верстка Главной 241115_17_00

--- assets/js/local.js
+++ assets/js/local.js
@@ -4,4 +4,29 @@
  * and open the template in the editor.
  */
 
+$(document).ready(function ( ) {
+    
+    if ($('#site').length > 0) {
+        var PageHeight = $(window).height();
+        var HeaderHeight = $('#header').height();
+        var FooterHeight = $('#footer').height();
+        var SiteHeight = PageHeight - HeaderHeight - FooterHeight;
+        $('#site').css('min-height', SiteHeight);
+    }
+    
+    if ($('.request').length > 0) {
+        $('.request').hover(function () {
+
+            $(this).children('.request-round').css('background-color', '#0B4A81');
+        }, function () {
+            $(this).children('.request-round').css('background-color', '#007AC2');
+        });
+    }
+
+   
+
+
+
+});
+
 
