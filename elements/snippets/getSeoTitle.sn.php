9d36012b4090251d357e831263f2c6fc659e1e4e
sharpeenko <web_eff@mail.ru>
2015-12-23 11:10:05 +0300

    Шаблоны 231215_1109

---
+++ elements/snippets/getSeoTitle.sn.php
@@ -0,0 +1,28 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$id = $modx->resource->get('id');
+
+$where = array(
+    'contentid' => $id,
+    'tmplvarid' => 2
+);
+
+$output = '';
+
+
+$tv = $modx->getObject('modTemplateVarResource', $where);
+
+if (!empty($tv)) {
+    $output = $tv->get('value');
+}
+else {
+   $output = $modx->resource->get('pagetitle'); 
+}
+
+
+return $output;
