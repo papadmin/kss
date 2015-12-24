80725261fc31584af19e941fdb4e33437fdfa6e3
sharpeenko <web_eff@mail.ru>
2015-12-24 21:39:15 +0300

    Шаблон Объект 241215_2139

---
+++ elements/snippets/lastNumber.sn.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$output = substr(str_replace('.', '', $input), -4);
+return $output;
+
