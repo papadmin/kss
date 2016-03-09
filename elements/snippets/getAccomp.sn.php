215a27eb8b4dff3566c4489d2fe1dd497c87e0dd
sharpeenko <web_eff@mail.ru>
2016-03-09 11:00:18 +0300

    Доработки 090316_1100

---
+++ elements/snippets/getAccomp.sn.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$id = $modx->resource->get('id');
+
+$q = $modx->newQuery('msProductLink', array('link' => 1, 'master' => $id));
+$q->select('slave');
+$resources = array();
+$output = '';
+if ($q->prepare() && $q->stmt->execute()) {
+    $ids = $q->stmt->fetchAll(PDO::FETCH_COLUMN);
+
+    foreach ($ids as $key => $value) {
+        $res = $modx->getObject('modResource', $value);
+        $resname = $res->get('pagetitle');
+        $reslink = $res->get('alias') . '.html';
+        $resimage = $res->get('image');
+        $output .= "<a href=\" $reslink\">
+            <img src=\"$resimage\"/>
+            <p>" . $resname . "</p>
+        </a>";
+    }
+}
+
+//if (!$output) {
+//$output = "573';
+//}
+
+echo $output;
+
