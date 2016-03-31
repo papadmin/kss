ea7d00fe2b69a340a8369858990d6e95c3bc4dd7
sharpeenko <web_eff@mail.ru>
2016-03-31 10:17:58 +0300

    Доработки 310316_1017

--- elements/snippets/getLastArticle.sn.php
+++ elements/snippets/getLastArticle.sn.php
@@ -6,11 +6,10 @@
  * and open the template in the editor.
  */
 
-$output = '';
+$output = '';  //строка для вывода чанка
 
-$id = $modx->resource->get('id');
-
-$linkId = array();
+$id = $modx->resource->get('id'); //текущий ресурс
+//$output = getArticles($id, $modx);
 
 $where = array(
     'value' => $id
@@ -18,27 +17,49 @@ $where = array(
 );
 $tv = $modx->getCollection('modTemplateVarResource', $where);
 
-
-
-
-foreach ($tv as $value) {
-
-
-    $linkId[] = $value->get('contentid');
+if ($tv) {
+    echo getLastArticleThis($id, $modx);
+} else {
+    getParents($id, $modx);
 }
 
+// Функция поиска родителя.
 
+function getParents($resid, $modx) {
+    $page = $modx->getObject('modResource', $resid);
+    $parentid = $page->get('parent'); // определяем родителя
 
-$criteria = $modx->newQuery('modResource');
-$criteria->where(array(
-    'id:IN' => $linkId
-));
-$criteria->sortby('publishedon', 'DESC');
-$criteria->limit(1);
+    if ($parentid) {
+        if (getArticlesTest($parentid, $modx)) {
+            echo getLastArticleThis($parentid, $modx);
+        } else {
+            getParents($parentid, $modx);
+        }
+    } else {
 
-$res = $modx->getObject('modResource', $criteria);
+        echo getLastArticleSource($modx);
+    }
+}
 
-if (!$res) {
+//Функция поиска статей по ресурсу
+
+function getArticlesTest($resid, $modx) {
+    $where = array(
+        'value' => $resid
+        , 'tmplvarid' => 52
+    );
+    $tv = $modx->getCollection('modTemplateVarResource', $where);
+
+    if ($tv) {
+        return $resid;
+    } else {
+        return false;
+    }
+}
+
+// Функция вывода крайней статьи по всему сайту
+
+function getLastArticleSource($modx) {
     $criteria = $modx->newQuery('modResource');
     $criteria->where(array(
         'parent' => 334
@@ -59,27 +80,47 @@ if (!$res) {
     $tv = $modx->getObject('modTemplateVarResource', $where);
     $resimage = $tv->get('value');
 
-    $output .= "<a class='dir-block-produstion' href=\" $reslink\">
-    <img src=\"$resimage\"/>
-    <p class='text-dir-block'>" . $resname . "</p>
-</a>";
-} else {
-    $resname = $res->get('longtitle');
-    $reslink = $res->get('alias') . '.html';
-
-    $id = $res->get('id');
-    $where = array(
-        'contentid' => $id
-        , 'tmplvarid' => 3
-    );
-    $tv = $modx->getObject('modTemplateVarResource', $where);
-    $resimage = $tv->get('value');
-
-    $output .= "<a class='dir-block-produstion' href=\" $reslink\">
+    return "<a class='dir-block-produstion' href=\" $reslink\">
     <img src=\"$resimage\"/>
     <p class='text-dir-block'>" . $resname . "</p>
 </a>";
 }
 
+// Функция вывода крайней статьи по ресурсу
 
-return $output;
+function getLastArticleThis($id, $modx) {
+    $linkId = array();
+
+    $where = array(
+        'value' => $id
+        , 'tmplvarid' => 52
+    );
+    $tv = $modx->getCollection('modTemplateVarResource', $where);
+    if ($tv) {
+        foreach ($tv as $value) {
+            $linkId[] = $value->get('contentid');
+        }
+        $criteria = $modx->newQuery('modResource');
+        $criteria->where(array(
+            'id:IN' => $linkId
+        ));
+        $criteria->sortby('publishedon', 'DESC');
+        $criteria->limit(1);
+        $res = $modx->getObject('modResource', $criteria);
+        $resname = $res->get('longtitle');
+        $reslink = $res->get('alias') . '.html';
+
+        $id = $res->get('id');
+        $where = array(
+            'contentid' => $id
+            , 'tmplvarid' => 3
+        );
+        $tv = $modx->getObject('modTemplateVarResource', $where);
+        $resimage = $tv->get('value');
+
+        return "<a class='dir-block-produstion' href=\" $reslink\">
+    <img src=\"$resimage\"/>
+    <p class='text-dir-block'>" . $resname . "</p>
+</a>";
+    }
+}
